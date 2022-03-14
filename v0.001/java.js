96eab1cdc1c1e18ee007e7c662f3b0f8e51036b4
REMIN T ROY <86014784+remindev@users.noreply.github.com>
2022-03-14 23:48:58 +0530

    Add files via upload

---
+++ v0.001/java.js
@@ -0,0 +1,13 @@
+const appMain = {
+    search:{
+        init:function(){
+          console.log("init");
+        },
+        find:function(a){
+            console.log("find : "+a);
+        },
+        exit:function(){
+            console.log("exit");
+        }
+    }
+}
