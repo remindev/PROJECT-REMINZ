b019f3c62eb6f56fa50d2d0479ef7b376449e068
REMIN T ROY <86014784+remindev@users.noreply.github.com>
2022-03-16 00:02:02 +0530

    Add files via upload

--- v0.001/java.js
+++ v0.001/java.js
@@ -1,13 +1,32 @@
 const appMain = {
     search:{
-        init:function(){
-          console.log("init");
+        disp:function(){ // init needed "disp()"
+
+        var searchInput = document.getElementById("top-body-cont-445-input");
+        var docContainer = document.getElementById("top-body-cont-445");
+        var docResultContainer = document.getElementById("top-body-44-search-results-ul");
+       
+        searchInput.addEventListener("focus",e=>{ // to display search result's box
+            docResultContainer.style.display = "initial";
+        });
+
+        document.addEventListener("click",e=>{// click outside so hide search output
+            var clickInsideElement = docContainer.contains(e.target);
+            if(!clickInsideElement){
+                docResultContainer.style.display = "none";
+            };
+        });
+
+        searchInput.addEventListener("keyup",e=>{ // get search results
+            this.results(e.target.value);
+        });
+
         },
-        find:function(a){
-            console.log("find : "+a);
-        },
-        exit:function(){
-            console.log("exit");
+        results:function(a){
+            console.log("target : "+a);
         }
+    },
+    init:function(){
+        this.search.disp();
     }
 }
